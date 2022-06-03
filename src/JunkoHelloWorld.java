ebc5268a45a928a4aeffdd671435fe62d962d3cd
junko <junkohuebner@gmail.com>
2022-06-02 22:22:37 -0500

    hi hi hi

---
+++ src/JunkoHelloWorld.java
@@ -0,0 +1,11 @@
+
+public class JunkoHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm Junko, hello world!");
+		System.out.println("I imported it from my local, now making a change to push it to Github!");
+		System.out.println("Making more change!");
+	}
+
+}
